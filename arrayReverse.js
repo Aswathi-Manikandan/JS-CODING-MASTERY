56222318921127aeb180138f3675b2b468059056
ahwathy <aswathiachus935@gmail.com>
2026-09-07 22:31:15 +0530

    revesed an array

---
+++ arrayReverse.js
@@ -0,0 +1,15 @@
+let arr = [10, 23, 90, 4]
+function func(arr) {
+    return arr.reverse()
+}
+console.log(func(arr));
+
+let arr1 = [10, 30, 3, 54]
+function func1(arr1) {
+    let out = []
+    for (let i = arr1.length - 1; i >= 0; i--) {
+        out.push(arr1[i])
+    }
+    return out
+}
+console.log(func1(arr1));
