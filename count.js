e15b5332bc30ad180bd14d4e189a59eca9b65ed4
ahwathy <aswathiachus935@gmail.com>
2026-09-07 15:04:15 +0530

    count total owels

--- count.js
+++ count.js
@@ -6,6 +6,7 @@ function count(ch) {
 }
 console.log(count(ch));
 
+
 let ch1 = "javascript"
 function count1(ch1) {
     let total = 0
@@ -15,3 +16,19 @@ function count1(ch1) {
     return total
 }
 console.log(count1(ch1));
+
+
+//COUNT VOWELS
+let ch2 = "javascript"
+function vo(ch2) {
+    let co = 0
+    let vow = 'aeiouAEIOU'
+    for (let i = 0; i < ch2.length; i++) {
+        if (vow.includes(ch[i])) {
+            co++
+        }
+    }
+    return co
+}
+console.log(vo(ch2));
+
