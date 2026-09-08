cfc7ce01d7f5b4a70288ec523bfd1a583722965d
ahwathy <aswathiachus935@gmail.com>
2026-09-08 10:35:19 +0530

    removed duplicates

---
+++ duplicate.js
@@ -0,0 +1,20 @@
+let arr = [1, 2, 2, 4, 5, 7, 8]
+function func(arr) {
+
+    let arq = []
+    for (let i = 0; i < arr.length; i++) {
+        let count = 0
+        for (let j = 0; j < arr.length; j++) {
+            if (arr[i] === arr[j]) {
+                count++
+            }
+        }
+        if (count < 2) {
+            arq.push(arr[i])
+        }
+
+    }
+    return arq
+
+}
+console.log(func(arr));
