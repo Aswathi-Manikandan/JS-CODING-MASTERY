8339b8881cc73b3d2815a852e6a4abeab3605717
ahwathy <aswathiachus935@gmail.com>
2026-09-14 11:23:31 +0530

    negative number

---
+++ negative.js
@@ -0,0 +1,22 @@
+let arr = [100, -200, 300, -400, -800]
+function neg(arr) {
+    let arr2 = []
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] < 0) {
+            arr2.push(arr[i])
+
+        }
+    }
+    return arr2
+}
+console.log(neg(arr));
+
+let arr3 = [100, -200, 300, -400, -800]
+let out = arr3.filter((x) => x < 0)
+console.log(out);
+
+
+let arr4 = [100, -200, -300, 400]
+let out2 = arr4.forEach((x) => console.log(x < 0));
+ )
+
