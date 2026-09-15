178d83feec3d4151c0e4948370ce1da37d9157c5
ahwathy <aswathiachus935@gmail.com>
2026-09-15 14:56:09 +0530

    notes

--- demo.js
+++ demo.js
@@ -1,1 +1,8 @@
-console.log({} + []);
+function xyz() {
+    console.log(a);
+    var a = 100
+
+}
+let b = xyz()
+console.log(b);
+
--- logic.js
+++ logic.js
@@ -28,6 +28,14 @@ function abc() {
 }//undefined
 
 
+function xyz() {
+    console.log(a);
+    var a = 100
+
+}
+let b = xyz()
+console.log(b);
+
 
 
 
