eef5ee3cbe8506eda23cac7dbbf640f663840f08
ahwathy <aswathiachus935@gmail.com>
2026-09-18 04:49:14 +0530

    notes

--- demo.js
+++ demo.js
@@ -1,9 +1,2 @@
-const abc = {}
-abc.key = "hello"
-const abc2 = { ...abc }
-console.log(abc);//{ key: 'hello' }
-console.log(abc2);//{ key: 'hello' }
-abc2.key = "world"
-console.log(abc2);//{ key: 'world' }
-
-
+console.log(abc)
+let abc = 100;
